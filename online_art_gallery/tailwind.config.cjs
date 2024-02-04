e222088c6a46d4f5191c0c3559f3f2c621553180
noakr <noakr97@gmail.com>
2024-02-04 16:23:14 +0200

    Updating

--- online_art_gallery/tailwind.config.cjs
+++ online_art_gallery/tailwind.config.cjs
@@ -1,7 +1,15 @@
 module.exports = {
   content: ["./*.{html,js,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      color: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        }
+      }
+    },
   },
   plugins: [
     require("@tailwindcss/typography"),
